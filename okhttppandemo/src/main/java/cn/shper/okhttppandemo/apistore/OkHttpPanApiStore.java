615afefcb83f430f48fa02a8fe6f738164c9dfaf
Shper <me@shper.cn>
2017-03-24 18:55:24 +0800

    Add The RequestResponseParser Demo

--- okhttppandemo/src/main/java/cn/shper/okhttppandemo/apistore/OkHttpPanApiStore.java
+++ okhttppandemo/src/main/java/cn/shper/okhttppandemo/apistore/OkHttpPanApiStore.java
@@ -4,6 +4,7 @@ import cn.shper.okhttppan.OkHttpPan;
 import cn.shper.okhttppan.callback.HttpCallback;
 import cn.shper.okhttppandemo.entity.BaseResponse;
 import cn.shper.okhttppandemo.entity.WeatherInfo;
+import cn.shper.okhttppandemo.parser.MResponseParser;
 
 /**
  * Author: Shper
@@ -34,18 +35,19 @@ public class OkHttpPanApiStore {
      */
     public static void getWeather(String cityId, HttpCallback<WeatherInfo> callback) {
         OkHttpPan.get().url(WEATHER_URL + cityId + ".html")
+                .responseParser(new MResponseParser())
                 .build().enqueue(WeatherInfo.class, callback);
     }
 
     /**
      * 获取天气原始Json数据
      */
-    public static void getWeatherJson(String cityId, HttpCallback<String> callback){
+    public static void getWeatherJson(String cityId, HttpCallback<String> callback) {
         OkHttpPan.get().url(WEATHER_URL + cityId + ".html")
                 .build().enqueue(null, callback);
     }
 
-    public static void get12306(HttpCallback<String> callback){
+    public static void get12306(HttpCallback<String> callback) {
         OkHttpPan.get().url("https://kyfw.12306.cn/otn/")
                 .build().enqueue(String.class, callback);
     }
